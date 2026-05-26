f95603f1e1f4a8303eb63753770f139a0006af3a
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2026-05-25 18:07:35 -0700

    fix settings keys

--- settings.lua
+++ settings.lua
@@ -8,21 +8,37 @@ local const = require('lib.constants')
 
 data:extend {
 	{
-		type = "bool-setting",
-		name = "mk-train",
-		setting_type = "startup",
-		default_value = true,
+		-- Debug mode (framework dependency)
+		type = 'bool-setting',
+		name = Framework.PREFIX .. 'debug-mode',
+		order = 'az',
+		setting_type = 'startup',
+		default_value = false,
 	},
 	{
-		type = "bool-setting",
-		name = "mk-cargo",
-		setting_type = "startup",
+		type = 'bool-setting',
+		name = const.settings.mk_train,
+		setting_type = 'startup',
 		default_value = true,
+		order = 'a',
 	},
 	{
-		type = "bool-setting",
-		name = "mk-fluid",
-		setting_type = "startup",
+		type = 'bool-setting',
+		name = const.settings.mk_cargo,
+		setting_type = 'startup',
 		default_value = true,
-	}	
-})
+		order = 'b',
+	},
+	{
+		type = 'bool-setting',
+		name = const.settings.mk_fluid,
+		setting_type = 'startup',
+		default_value = true,
+		order = 'c',
+	},
+}
+
+------------------------------------------------------------------------
+
+---@diagnostic disable-next-line: undefined-field
+Framework.post_settings_stage()
