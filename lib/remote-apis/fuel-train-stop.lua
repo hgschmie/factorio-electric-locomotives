7aa302c694e21b220c86c243c6401918a049561b
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2026-05-30 15:36:35 -0700

    add framework support for remote apis

---
+++ lib/remote-apis/fuel-train-stop.lua
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- FuelTrainStop remote API
+--------------------------------------------------------------------------------
+
+local const = require('lib.constants')
+
+---@param api_name string
+local function exclude_locos(api_name)
+    for _, loco_name in pairs(const.locomotive_names) do
+        if remote.interfaces[api_name]['exclude_from_fuel_schedule'] then
+            remote.call(api_name, 'exclude_from_fuel_schedule', loco_name)
+        end
+    end
+end
+
+local FuelTrainStop = {
+    on_configuration_changed = exclude_locos,
+}
+
+return FuelTrainStop
