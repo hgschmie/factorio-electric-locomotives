5f13364e33008443270cfa77c080e84e945fc306
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2026-07-23 16:07:32 -0700

    fix speed and acceleration progression

--- scripts/locomotive.lua
+++ scripts/locomotive.lua
@@ -103,8 +103,8 @@ function Locomotive:refuel(engine)
     local remaining_fuel
     if engine.entity.burner.currently_burning then remaining_fuel = engine.entity.burner.remaining_burning_fuel end
 
-    engine.speed_tier = engine.speed_tier or determine_tier(engine.entity.force_index, const.technology_speed_prefix)
-    engine.acceleration_tier = engine.speed_tier or determine_tier(engine.entity.force_index, const.technology_acceleration_prefix)
+    engine.speed_tier = determine_tier(engine.entity.force_index, const.technology_speed_prefix)
+    engine.acceleration_tier = determine_tier(engine.entity.force_index, const.technology_acceleration_prefix)
 
     -- assign the right fuel
     engine.entity.burner.currently_burning = assert(prototypes.item[const:fuel_name(engine)])
